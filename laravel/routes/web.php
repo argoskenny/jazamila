f51082ce141725bd7ec242f9139c22ba7a6bcb66
Kenny <argoskenny@gmail.com>
2025-09-12 09:19:34 +0800

    docs: describe restaurant listing module

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticPageController;
+use App\Http\Controllers\JazamilaController;
 
+// Core site routes
+Route::get('/', [JazamilaController::class, 'index']);
+Route::get('/listdata/{location}/{type}/{max}/{min}/{page}', [JazamilaController::class, 'listdata']);
+Route::get('/detail/{id}', [JazamilaController::class, 'detail'])->whereNumber('id');
+Route::get('/jsonapi', [JazamilaController::class, 'jsonapi']);
+
+// Static pages
 Route::get('/map', [StaticPageController::class, 'map']);
 Route::get('/about', [StaticPageController::class, 'about']);
 Route::get('/post', [StaticPageController::class, 'post']);
 Route::get('/CaptchaImg', [StaticPageController::class, 'captchaImg']);
-
